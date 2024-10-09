d6edc972646b5e01f5df53378489c3a564a9c887
vikhil1912 <vikhil1912@gmail.com>
2024-10-09 23:57:26 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,32 @@
+var n1 = Math.random();
+n1=n1*6;
+n1=Math.floor(n1)+1;
+var n2 = Math.random();
+n2=n2*6;
+n2=Math.floor(n2)+1;
+if(n1>n2){
+    document.querySelector("h1").textContent="player1 wins";
+}
+else if(n1==n2){
+    document.querySelector("h1").textContent="tie";
+}
+else{
+    document.querySelector("h1").textContent="player2 wins";
+}
+fn1(n1);fn2(n2);
+function fn1(n){
+    if(n==1)document.querySelector("img.img1").setAttribute("src","./images/dice1.png");
+    if(n==2)document.querySelector("img.img1").setAttribute("src","./images/dice2.png");
+    if(n==3)document.querySelector("img.img1").setAttribute("src","./images/dice3.png");
+    if(n==4)document.querySelector("img.img1").setAttribute("src","./images/dice4.png");
+    if(n==5)document.querySelector("img.img1").setAttribute("src","./images/dice5.png");
+    if(n==6)document.querySelector("img.img1").setAttribute("src","./images/dice6.png");
+}
+function fn2(n){
+    if(n==1)document.querySelector("img.img2").setAttribute("src","./images/dice1.png");
+    if(n==2)document.querySelector("img.img2").setAttribute("src","./images/dice2.png");
+    if(n==3)document.querySelector("img.img2").setAttribute("src","./images/dice3.png");
+    if(n==4)document.querySelector("img.img2").setAttribute("src","./images/dice4.png");
+    if(n==5)document.querySelector("img.img2").setAttribute("src","./images/dice5.png");
+    if(n==6)document.querySelector("img.img2").setAttribute("src","./images/dice6.png");
+}
